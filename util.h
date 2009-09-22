b7f2f60ead23e2416e1f9f2431b61fff00115ffc
Bill Welliver <bill@welliver.org>
2009-09-22 01:14:49 +0000

    changes to get things working in pike 7.8
    --HG--
    extra : convert_revision : svn%3A2b5a5965-8a46-0410-aa19-a76fe43533fc/trunk%403463

--- util.h
+++ util.h
@@ -1,34 +1,12 @@
 /* Standard Pike include files. */
-#include "bignum.h"
-#include "array.h"
-#include "builtin_functions.h"
-#include "constants.h"
+#include "global.h"
 #include "interpret.h"
-#include "mapping.h"
-#include "multiset.h"
+#include "backend.h"
 #include "module_support.h"
 #include "object.h"
-#include "global.h"
-#include "pike_macros.h"
-#include "pike_types.h"
-#include "program.h"
-#include "stralloc.h"
-#include "svalue.h"
+#include "builtin_functions.h"
+#include "mapping.h"
 #include "threads.h"
-#include "version.h"
-#include "operators.h"
+#include "bignum.h"
 
-#if (PIKE_MAJOR_VERSION == 7 && PIKE_MINOR_VERSION == 1 && PIKE_BUILD_VERSION >= 12) || PIKE_MAJOR_VERSION > 7 || (PIKE_MAJOR_VERSION == 7 && PIKE_MINOR_VERSION > 1)
-# include "pike_error.h"
-#else
-# include "error.h"
-# ifndef Pike_error
-#  define Pike_error error
-# endif
-#endif
-
-#ifndef ARG
-/* Get argument # _n_ */
-#define ARG(_n_) Pike_sp[-((args - _n_) + 1)]
-#endif
 
